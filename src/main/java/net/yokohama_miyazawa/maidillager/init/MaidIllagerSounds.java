ffcb691e118d7c1409a2ab168511d39613d88ae9
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2023-07-19 21:26:21 +0900

    Minecraft本体のSoundEvent実装を参考にした

--- src/main/java/net/yokohama_miyazawa/maidillager/init/MaidIllagerSounds.java
+++ src/main/java/net/yokohama_miyazawa/maidillager/init/MaidIllagerSounds.java
@@ -3,28 +3,30 @@ package net.yokohama_miyazawa.maidillager.init;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.yokohama_miyazawa.maidillager.MaidIllager;
 
 public class MaidIllagerSounds {
-    public static final Identifier MAID_PILLAGER_AMBIENT_ID = new Identifier(makeId(MaidIllager.MODID, "entity.pillager.ambient"));
-    public static final Identifier MAID_PILLAGER_CELEBRATE_ID = new Identifier(makeId(MaidIllager.MODID, "entity.pillager.celebrate"));
-    public static final Identifier MAID_PILLAGER_DEATH_ID = new Identifier(makeId(MaidIllager.MODID, "entity.pillager.death"));
-    public static final Identifier MAID_PILLAGER_HURT_ID = new Identifier(makeId(MaidIllager.MODID, "entity.pillager.hurt"));
+    public static SoundEvent MAID_PILLAGER_AMBIENT;
+    public static SoundEvent MAID_PILLAGER_CELEBRATE;
+    public static SoundEvent MAID_PILLAGER_DEATH;
+    public static SoundEvent MAID_PILLAGER_HURT;
 
-    public static SoundEvent MAID_PILLAGER_AMBIENT = SoundEvent.of(MAID_PILLAGER_AMBIENT_ID);
-    public static SoundEvent MAID_PILLAGER_CELEBRATE = SoundEvent.of(MAID_PILLAGER_CELEBRATE_ID);
-    public static SoundEvent MAID_PILLAGER_DEATH = SoundEvent.of(MAID_PILLAGER_DEATH_ID);
-    public static SoundEvent MAID_PILLAGER_HURT = SoundEvent.of(MAID_PILLAGER_HURT_ID);
+    public static void onInitialize() {
+        MAID_PILLAGER_AMBIENT = register("entity.pillager.ambient");
+        MAID_PILLAGER_CELEBRATE = register("entity.pillager.celebrate");
+        MAID_PILLAGER_DEATH = register("entity.pillager.death");
+        MAID_PILLAGER_HURT = register("entity.pillager.hurt");
+    }
+
+    private static SoundEvent register(String soundId) {
+        Identifier id = new Identifier(makeId(MaidIllager.MODID, soundId));
+        SoundEvent soundEvent = SoundEvent.of(id);
+        return Registry.register(Registries.SOUND_EVENT, id, soundEvent);
+    }
 
     private static String makeId(String modId, String sound) {
         return modId + ':' + sound;
-    }
-
-    public static void onInitialize() {
-        Registry.register(Registries.SOUND_EVENT, MaidIllagerSounds.MAID_PILLAGER_AMBIENT_ID, MAID_PILLAGER_AMBIENT);
-        Registry.register(Registries.SOUND_EVENT, MaidIllagerSounds.MAID_PILLAGER_CELEBRATE_ID, MAID_PILLAGER_CELEBRATE);
-        Registry.register(Registries.SOUND_EVENT, MaidIllagerSounds.MAID_PILLAGER_DEATH_ID, MAID_PILLAGER_DEATH);
-        Registry.register(Registries.SOUND_EVENT, MaidIllagerSounds.MAID_PILLAGER_HURT_ID, MAID_PILLAGER_HURT);
     }
 }
