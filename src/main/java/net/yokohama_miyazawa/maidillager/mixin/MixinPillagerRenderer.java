2d16f1ea362523771a4b682b85ab7f91c8ce4106
Osamu Miyazawa <miyazawa.o.aa@m.titech.ac.jp>
2023-12-15 19:21:56 +0900

    テクスチャ変更機能を実装。

--- src/main/java/net/yokohama_miyazawa/maidillager/mixin/MixinPillagerRenderer.java
+++ src/main/java/net/yokohama_miyazawa/maidillager/mixin/MixinPillagerRenderer.java
@@ -11,10 +11,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.util.Identifier;
 
+import static net.yokohama_miyazawa.maidillager.config.ModConfigs.UMU_LIKE;
+
 @Environment(EnvType.CLIENT)
 @Mixin(PillagerEntityRenderer.class)
 public class MixinPillagerRenderer {
-    private static final Identifier PILLAGER = new Identifier(MaidIllager.MODID + ":textures/entity/maid_pillager.png");
+    private static final String TEXTURE = UMU_LIKE ? ":textures/entity/maid_pillager_umu.png" : ":textures/entity/maid_pillager.png";
+    private static final Identifier PILLAGER = new Identifier(MaidIllager.MODID + TEXTURE);
 
     @Inject(method = "getTexture", at = @At("RETURN"), cancellable = true)
     public void onGetTexture(PillagerEntity entity, CallbackInfoReturnable<Identifier> cir){
