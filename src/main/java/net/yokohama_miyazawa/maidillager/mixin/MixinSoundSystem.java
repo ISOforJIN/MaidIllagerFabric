b1afe8a6e1854fe0d865b827b76619e7f3ab97dd
ren <0806renkun@gmail.com>
2024-01-15 19:02:55 +0900

    内部構造を変更(細かなバグ修正)

--- src/main/java/net/yokohama_miyazawa/maidillager/mixin/MixinSoundSystem.java
+++ src/main/java/net/yokohama_miyazawa/maidillager/mixin/MixinSoundSystem.java
@@ -1,19 +1,9 @@
 package net.yokohama_miyazawa.maidillager.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.sound.*;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.floatprovider.ConstantFloatProvider;
-import net.minecraft.util.math.random.Random;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
